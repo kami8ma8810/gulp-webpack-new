fc18fb64d919d1659dd20a27c52d4cf74dbd22c8
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-24 16:17:59 +0900

    babel対応

--- _static/src/js/sub.js
+++ _static/src/js/sub.js
@@ -2,4 +2,6 @@
 export function hello() {
 	alert("はろー");
 	console.log("helloメソッドが実行されました！");
+	console.log("hello2メソッドが実行されました！");
+	console.log("hello3メソッドが実行されました！");
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,29 @@ const path = require('path');
 
 module.exports = {
 	//開発（ソースマップ有効）
-	mode: 'development',
+	// mode: 'development',
 	//本番公開
-	// mode: 'production',
-
-	// ローカルサーバー
-	devServer: {
-		contentBase: './_static/dist',
-		//openは自動で立ち上げる
-		open: true
-	},
+	mode: 'production',
 
 	// メインとなるJavaScriptファイル（エントリーポイント）
 	entry: './_static/src/js/index.js',
+
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						// プリセット指定でES2020をES5に変換
+						'@babel/preset-env',
+					],
+				},
+			}, ],
+		}, ],
+	},
+	// ES5（IE11など）向けの指定
+	target: ['web', 'es5'],
 
 	// ファイルの出力設定
 	output: {
@@ -22,5 +32,19 @@ module.exports = {
 		path: path.resolve(__dirname, '_static', 'dist'),
 		// 出力ファイル名
 		filename: 'bundle.js'
-	}
+	},
+
+	// ローカルサーバー
+	devServer: {
+		contentBase: path.resolve(__dirname, '_static', 'dist'),
+		index: 'index.html',
+		//openは自動で立ち上げる
+		open: true,
+		//コンパイラエラーまたは警告がある場合にブラウザに全画面オーバーレイを表示
+		overlay: {
+			warnings: true,
+			errors: true,
+		},
+	},
+
 };
