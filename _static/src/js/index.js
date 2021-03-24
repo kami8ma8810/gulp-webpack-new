bb3eb7be697944e5ff02cbd5f044c2e67e400df6
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-24 16:29:41 +0900

    jquery追加

--- _static/src/js/index.js
+++ _static/src/js/index.js
@@ -1,9 +1,3 @@
-// import 文を使って sub.js ファイルを読み込む。
-import {
-	hello
-} from "./sub";
-
-// sub.jsに定義されたJavaScriptを実行する。
-hello();
+import './myjQuery';
 
 console.log('this is main js.')
--- _static/src/js/sub.js
+++
@@ -1,7 +0,0 @@
-// export文を使ってhello関数を定義する。
-export function hello() {
-	alert("はろー");
-	console.log("helloメソッドが実行されました！");
-	console.log("hello2メソッドが実行されました！");
-	console.log("hello3メソッドが実行されました！");
-}
