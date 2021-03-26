df005612b7a99c19207676db305f978d9842769a
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-26 13:29:07 +0900

    generator-runtime/core-js@3インストール

--- _static/src/js/index.js
+++ _static/src/js/index.js
@@ -1,3 +1,32 @@
 import './myjQuery';
+import 'regenerator-runtime';
 
-console.log('this is main js.')
+const init = async () => {
+	console.log('this is main js.');
+	await asyncFn();
+}
+// ------------------
+// 以下サンブル文
+// ------------------
+async function asyncFn() {
+	console.log('Im async function.');
+}
+init();
+
+function sampleResolve(value) {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(value * 2);
+		}, 5000);
+	})
+}
+
+function sample() {
+	return sampleResolve(5).then(result => {
+		return result + 5;
+	});
+}
+
+sample().then(result => {
+	console.log(result); // => 15
+});
--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,14 @@
 module.exports = function (api) {
 	api.cache(true);
 
-	const presets = [
-		// プリセット指定でES2020をES5に変換
-		'@babel/preset-env',
-		{
-			'modules': false,
-			'targets': ['web', 'es5'],
-		},
-	];
-
 	return {
-		presets,
-	};
+		'presets': [
+			['@babel/preset-env', {
+				'targets': [
+					'> 1%',
+					'ie 11'
+				]
+			}]
+		]
+	}
 }
