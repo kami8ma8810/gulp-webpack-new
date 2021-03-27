8a612ce82e4ef82ff543d86e85d4c53691daa8a1
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-27 20:55:23 +0900

    rimrafで詰まったので戻した

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 		//  出力ファイルのディレクトリ名 resolveは絶対パス
 		path: path.resolve(__dirname, '_static', 'dist'),
 		// 出力ファイル名
-		filename: 'bundle.js'
+		filename: '[name].[chunkhash].js'
 	},
 
 	module: {
