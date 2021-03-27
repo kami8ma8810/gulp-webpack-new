92921d5ba87696078fd47c2e40d9521b4cea5e20
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-27 19:38:08 +0900

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	//開発（ソースマップ有効）
@@ -6,14 +7,14 @@ module.exports = {
 	//本番公開
 	mode: 'production',
 
-	cache: {
-		type: 'filesystem',
-		buildDependencies: {
-			config: [__filename]
-		}
-	},
-	// メインとなるJavaScriptファイル（エントリーポイント）
 	entry: './_static/src/js/index.js',
+	// ファイルの出力設定
+	output: {
+		//  出力ファイルのディレクトリ名 resolveは絶対パス
+		path: path.resolve(__dirname, '_static', 'dist'),
+		// 出力ファイル名
+		filename: 'bundle.js'
+	},
 
 	module: {
 		rules: [
@@ -40,14 +41,12 @@ module.exports = {
 			},
 		],
 	},
-
-	// ファイルの出力設定
-	output: {
-		//  出力ファイルのディレクトリ名 resolveは絶対パス
-		path: path.resolve(__dirname, '_static', 'dist'),
-		// 出力ファイル名
-		filename: 'bundle.js'
-	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: './_static/src/index.html',
+			inject: 'body'
+		})
+	],
 
 	// ローカルサーバー
 	devServer: {
@@ -64,5 +63,11 @@ module.exports = {
 	performance: {
 		maxEntrypointSize: 500000,
 		maxAssetSize: 500000,
+	},
+	cache: {
+		type: 'filesystem',
+		buildDependencies: {
+			config: [__filename]
+		}
 	},
 };
